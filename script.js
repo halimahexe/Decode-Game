b1b3eddde268f60813d27559f1fa53400cf75b19
halimahexe <127022084+halimahexe@users.noreply.github.com>
2023-11-20 22:49:22 +0000

    adding divs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const strings = [{
     string: "The quick brown fox jumped over the lazy dog.",
 }, {
-
+    string: ""
 }, {
 
 }, {
@@ -15,3 +15,9 @@ const strings = [{
 }, {
 
 }, ]
+
+let string = strings[0].string;
+let encode = string.split('');
+console.log(encode);
+
+// need regex to only encode characters that match A-Za-z
